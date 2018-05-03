5fe93e49859d4ff7955a58c8e2e0cd2a4d38b582
Jan van Dieken <Zanthelo@users.noreply.github.com>
2018-05-03 13:44:34 +0200

    change socket to work in windows

--- src/smtp-server/main.cpp
+++ src/smtp-server/main.cpp
@@ -1,41 +1,67 @@
-#include <netinet/in.h>
+
 #include <cstdio>
 #include <cstring>
+
+#ifdef WIN32
+#include <WinSock2.h>
+#else
 #include <unistd.h>
+#include <netinet/in.h>
+#endif
 
 #define SERVER_BUFFER_SIZE      1024
 #define PORT 5555
 
 int main() {
-  int socketId = socket(PF_INET, SOCK_STREAM, 0);
+#ifdef WIN32
+  WSAData wsaData = { 0 };
+  WSAStartup(MAKEWORD(2, 2), &wsaData);
+#endif
+  
+  auto socketId = socket(PF_INET, SOCK_STREAM, 0);
 
-  struct sockaddr_in serverAddr;
-  bzero((char*) &serverAddr, sizeof(serverAddr));
+  sockaddr_in serverAddr = { 0 };
   serverAddr.sin_family = AF_INET;
   serverAddr.sin_port = htons(PORT);
   serverAddr.sin_addr.s_addr = INADDR_ANY;
-  bind(socketId, (struct sockaddr*) &serverAddr, sizeof(serverAddr));
+  auto error = bind(socketId, (sockaddr*) &serverAddr, sizeof(serverAddr));
 
-  listen(socketId, 5);
+  error = listen(socketId, 5);
 
   int finished = 0;
   while (!finished) {
-    struct sockaddr_storage serverStorage;
-    socklen_t addr_size = sizeof serverStorage;
-    int connection = accept(socketId, (struct sockaddr*) &serverStorage, &addr_size);
+    sockaddr_storage serverStorage;
+    int addr_size = sizeof(serverStorage);
+    auto connection = accept(socketId, (sockaddr*) &serverStorage, &addr_size);
 
     char buffer[SERVER_BUFFER_SIZE];
-    int get = read(connection, buffer, SERVER_BUFFER_SIZE - 1);
+#ifdef WIN32
+    auto get = recv(connection, buffer, SERVER_BUFFER_SIZE - 1, 0);
+#else
+    auto get = read(connection, buffer, SERVER_BUFFER_SIZE - 1);
+#endif
 
     buffer[get] = '\0';
     fprintf(stdout, "%s\n", buffer);
 
+#ifdef WIN32
+    send(connection, "OK", 2, 0);
+#else
     write(connection, "OK", 2);
+#endif
 
     fprintf(stdout, "Message Complete\n");
 
+#ifdef WIN32
+    closesocket(connection);
+#else
     close(connection);
+#endif
   }
 
+#ifdef WIN32
+  closesocket(socketId);
+#else
   close(socketId);
+#endif
 }
