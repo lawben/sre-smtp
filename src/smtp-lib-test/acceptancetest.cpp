5ec4f192a8715eca31f14c6683eaf313bcd278cc
martin <martin.fischer@student.hpi.de>
2018-05-12 18:07:32 +0200

    implement unix socket connect

--- src/smtp-lib-test/acceptancetest.cpp
+++ src/smtp-lib-test/acceptancetest.cpp
@@ -1,10 +1,41 @@
 #include "catch/catch.hpp"
 
 #include "smtp-lib/smtp_server.hpp"
+#include "smtp-lib/raw_socket.hpp"
+#include "smtp-lib/connection.hpp"
+
+bool string_starts_with(std::string& s, std::string& prefix) {
+	return s.find(prefix) == 0;
+}
 
 TEST_CASE("Send valid mails", "[Message Receiving]") {
+	uint16_t in_port = 5556;
+	uint16_t out_port = 5557;
+	auto socket = std::make_unique<RawSocket>(RawSocket::new_socket());
+	
+	SMTPServer server(in_port);
+	server.run();
+	
+	socket->bind(out_port);
+	socket->connect(std::string("127.0.0.1"), in_port);
+	auto connection = std::make_unique<Connection>(std::move(socket));
+	
+	{
+		const auto bytes = connection->read();
+		std::string result(bytes.begin(), bytes.end());
+		REQUIRE(string_starts_with(result, std::string("220")));
+	}
 
-	REQUIRE(false);	
+	std::string helo_message("HELO 127.0.0.1");
+	connection->write(Bytes(helo_message.begin(), helo_message.end()));
+
+	{
+		const auto bytes = connection->read();
+		std::string result(bytes.begin(), bytes.end());
+		REQUIRE(string_starts_with(result, std::string("250")));
+	}
+
+	server.stop();
 }
 
 TEST_CASE("Send invalid mails", "[Message Receiving]") {
