a8cdb8fba46879f7c1b335b45bf095749d3c3213
martin <martin.fischer@student.hpi.de>
2018-05-12 19:08:16 +0200

    remove unused function

--- src/smtp-lib/mail_receiver.hpp
+++ src/smtp-lib/mail_receiver.hpp
@@ -10,8 +10,6 @@ class MailReceiver : public NonCopyable {
     // This is the main SMTP loop. This assumes a valid connection with read/write. It will run in a separate thread.
     void run();
 
-    RawSocket* get_socket() { return m_connection->get_socket(); }
-
   private:
     std::unique_ptr<Connection> m_connection;
     MailParser m_parser;
