42724c71ab981179da57099ea0048d1ad0f71f1a
martin <martin.fischer@student.hpi.de>
2018-05-12 15:12:22 +0200

    adjust server test

--- src/smtp-lib-test/servertest.cpp
+++ src/smtp-lib-test/servertest.cpp
@@ -3,11 +3,11 @@
 #include "smtp-lib/smtp_server.hpp"
 
 TEST_CASE("Open, run and close server", "[SMTPServer]") {
-	auto server = SMTPServer(8080);
+	SMTPServer server(8080);
 
 	REQUIRE(server.is_running() == false);
-	//server.run();
+	server.run();
 	REQUIRE(server.is_running());
-	//server.stop();
+	server.stop();
 	REQUIRE(server.is_running() == false);
 }
