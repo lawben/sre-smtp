faaf53432f318092308c2db9309e3af00e6bd8ff
martin <martin.fischer@student.hpi.de>
2018-05-17 19:18:45 +0200

    split up socket listener test

--- src/smtp-lib-test/socket_listener_test.cpp
+++ src/smtp-lib-test/socket_listener_test.cpp
@@ -5,7 +5,7 @@
 
 #include "smtp-lib/socket.hpp"
 
-TEST_CASE("accept listener", "[socket_listener]") {
+TEST_CASE("accept with no listener", "[socket_listener]") {
 
 	uint16_t port = 5556;
 	Socket socket(port);
@@ -13,26 +13,33 @@ TEST_CASE("accept listener", "[socket_listener]") {
 		auto connection = socket.accept_connection();
 		CHECK_FALSE(connection->is_valid());
 	}
+}
 
-	auto sender = std::make_unique<RawSocket>(RawSocket::new_socket());
-	uint16_t sender_port = 5555;
-	CHECK(sender->bind(sender_port));
+TEST_CASE("accept listeners", "[socket_listener]") {
 
-	std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	uint16_t port = 5556;
+	Socket socket(port);
 
-	CHECK(sender->connect(std::string("127.0.0.1"), port));
 	{
-		auto receiver_connection = socket.accept_connection();
-		CHECK(receiver_connection->is_valid());
-
-		std::string to_send = "test";
-
-		CHECK(sender->write(to_send));
-
+		auto sender = std::make_unique<RawSocket>(RawSocket::new_socket());
+		uint16_t sender_port = 5555;
+		CHECK(sender->bind(sender_port));
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
-		auto bytes = receiver_connection->read();
-		std::string message(bytes.begin(), bytes.end());
-		CHECK(to_send == message);
+		CHECK(sender->connect(std::string("127.0.0.1"), port));
 	}
+	{
+		auto sender = std::make_unique<RawSocket>(RawSocket::new_socket());
+		uint16_t sender_port = 5555;
+		CHECK(sender->bind(sender_port));
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		CHECK(sender->connect(std::string("127.0.0.1"), port));
+	}
+	{
+		auto sender = std::make_unique<RawSocket>(RawSocket::new_socket());
+		uint16_t sender_port = 5555;
+		CHECK(sender->bind(sender_port));
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		CHECK(sender->connect(std::string("127.0.0.1"), port));
+	}
+
 }
