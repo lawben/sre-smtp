c85741dea17dbde8f236f40e07b191b0b3b77d57
martin <martin.fischer@student.hpi.de>
2018-05-20 00:17:23 +0200

    refactor socket listener tests

--- src/smtp-lib-test/socket_listener_test.cpp
+++ src/smtp-lib-test/socket_listener_test.cpp
@@ -11,10 +11,10 @@ TEST_CASE("accept with no listener", "[socket_listener]") {
 
 	uint16_t port = 5556;
 	SocketListener socket(port);
-	{
-		auto connection = socket.accept_connection();
-		CHECK_FALSE(connection->is_valid());
-	}
+
+	auto connection = socket.accept_connection();
+
+	CHECK_FALSE(connection->is_valid());
 }
 
 TEST_CASE("accept listeners", "[socket_listener]") {
@@ -23,40 +23,28 @@ TEST_CASE("accept listeners", "[socket_listener]") {
 	std::string host = "127.0.0.1";
 	SocketListener socket(port);
 
-	uint16_t sender_port = 5555;
+	uint16_t sender_port_1 = 5557;
+	uint16_t sender_port_2 = 5558;
+	uint16_t sender_port_3 = 5559;
 
-	{
-		auto sender = std::make_unique<RawSocket>(RawSocket::new_socket());
-		CHECK(sender->bind(sender_port));
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
-		sender->connect(std::string(host), port);
+	auto sender_1 = std::make_unique<RawSocket>(RawSocket::new_socket(sender_port_1));
+	auto sender_2 = std::make_unique<RawSocket>(RawSocket::new_socket(sender_port_2));
+	auto sender_3 = std::make_unique<RawSocket>(RawSocket::new_socket(sender_port_3));
+	
+	sender_1->connect(std::string(host), port);
+	sender_2->connect(std::string(host), port);
+	sender_3->connect(std::string(host), port);
 
-		wait_for_network_interaction();
-		
-		auto connection = socket.accept_connection();
-		CHECK(connection->is_valid());
-	}
-	{
-		auto sender = std::make_unique<RawSocket>(RawSocket::new_socket());
-		CHECK(sender->bind(sender_port));
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
-		sender->connect(std::string(host), port);
+	wait_for_network_interaction();
+	
+	auto connection_1 = socket.accept_connection();
+	auto connection_2 = socket.accept_connection();
+	auto connection_3 = socket.accept_connection();
+	auto connection_4 = socket.accept_connection();
 
-		wait_for_network_interaction();
+	CHECK(connection_1->is_valid());
+	CHECK(connection_2->is_valid());
+	CHECK(connection_3->is_valid());
 
-		auto connection = socket.accept_connection();
-		CHECK(connection->is_valid());
-	}
-	{
-		auto sender = std::make_unique<RawSocket>(RawSocket::new_socket());
-		CHECK(sender->bind(sender_port));
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
-		sender->connect(std::string(host), port);
-
-		wait_for_network_interaction();
-
-		auto connection = socket.accept_connection();
-		CHECK(connection->is_valid());
-	}
-
+	CHECK_FALSE(connection_4->is_valid());
 }
