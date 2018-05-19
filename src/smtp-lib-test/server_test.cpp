2036e78bc3e0a8f888b10343371d5dbc4cd2639f
martin <martin.fischer@student.hpi.de>
2018-05-20 00:56:04 +0200

    refactor server test

--- src/smtp-lib-test/server_test.cpp
+++ src/smtp-lib-test/server_test.cpp
@@ -4,53 +4,50 @@
 #include "smtp-lib/raw_socket.hpp"
 #include "smtp-lib/connection.hpp"
 
-bool check_return_code_new(const std::unique_ptr<Connection>& connection, std::string prefix) {
-	std::vector<char> bytes;
-	while(bytes.empty())
-		bytes = connection->read();
-	std::string result(bytes.begin(), bytes.end());
-	return result.find(prefix) == 0;
-}
+#include "smtp-lib-test/helpers.hpp"
 
-TEST_CASE("Open, run and close server", "[SMTPServer]") {
+TEST_CASE("run server", "[SMTPServer]") {
     
 	SMTPServer server(8080);
 
-    REQUIRE(server.is_running() == false);
+    CHECK_FALSE(server.is_running());
     
 	auto server_thread = std::thread(&SMTPServer::run, &server);
-	
 	std::this_thread::sleep_for(std::chrono::milliseconds(10));
-	REQUIRE(server.is_running());
+	
+	CHECK(server.is_running());
 
     server.stop();
     
-	REQUIRE(server.is_running() == false);
+	CHECK_FALSE(server.is_running());
 
 	server_thread.join();
 }
 
 TEST_CASE("Stop server while client is connected", "[SMTPServer]") {
 
-	uint16_t in_port = 5572;
-	auto socket = std::make_unique<RawSocket>(RawSocket::new_socket());
-	SMTPServer server(in_port);
+	uint16_t server_port = 5555;
+	std::string server_address = "127.0.0.1";
+	SMTPServer server(server_port);
 	auto server_thread = std::thread(&SMTPServer::run, &server);
 
-	uint16_t out_port = 5573;
-	socket->bind(out_port);
-	std::string host = "127.0.0.1";
-	socket->connect(host, in_port);
-	auto connection = std::make_unique<Connection>(std::move(socket));
+	uint16_t client_port = 5556;
+	auto client = RawSocket::new_socket(client_port);
 
-	REQUIRE(server.is_running());
+	wait_for_network_interaction();
 
+	client.connect(server_address, server_port);
+	auto connection = std::make_unique<Connection>(std::make_unique<RawSocket>(std::move(client)));
+
+	wait_for_network_interaction();
+
+	CHECK(server.is_running());
 	CHECK(connection->is_valid());
-	CHECK(check_return_code_new(connection, "220"));
+	CHECK(check_return_code(connection, "220"));
 
 	server.stop();
 
-	REQUIRE(server.is_running() == false);
+	CHECK_FALSE(server.is_running());
 
 	server_thread.join();
 }
