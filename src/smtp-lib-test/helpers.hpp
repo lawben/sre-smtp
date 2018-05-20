15fb489db45144c44dbb497746242a2fe44d10ea
martin <martin.fischer@student.hpi.de>
2018-05-20 19:13:39 +0200

    add helper function for socket

--- src/smtp-lib-test/helpers.hpp
+++ src/smtp-lib-test/helpers.hpp
@@ -18,3 +18,11 @@ bool inline check_return_code(Connection& connection, std::string prefix) {
 	std::string result(bytes.begin(), bytes.end());
 	return result.find(prefix) == 0;
 }
+
+bool inline check_return_code(RawSocket& socket, std::string prefix) {
+	wait_for_network_interaction();
+
+	const auto bytes = socket.read(1024);
+	std::string result(bytes.begin(), bytes.end());
+	return result.find(prefix) == 0;
+}
