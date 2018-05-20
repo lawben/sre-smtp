4a2f64af2c55263c52d929e826be50c2188a0b9c
martin <martin.fischer@student.hpi.de>
2018-05-20 19:04:20 +0200

    do not call constructor inside connection test

--- src/smtp-lib-test/connection_test.cpp
+++ src/smtp-lib-test/connection_test.cpp
@@ -1,5 +1,7 @@
 #include "catch/catch.hpp"
 
+#include <vector>
+
 #include "smtp-lib/socket_listener.hpp"
 
 #include "smtp-lib-test/helpers.hpp"
@@ -47,18 +49,22 @@ TEST_CASE("free resources", "[connection]") {
 	uint16_t client_port = 5556;
 	std::string to_send = "Hello Client!";
 
+	std::vector<Connection> connections;
+
 	{
 		auto client = RawSocket::new_socket(client_port);
 		client.connect(listener_address, listener_port);
 
 		wait_for_network_interaction();
 
-		auto connection = listener.accept_connection();
-		connection.write(to_send);
+		connections.push_back(std::move(listener.accept_connection()));
+		connections.back().write(to_send);
 		wait_for_network_interaction();
 		auto bytes = client.read(1024);
 
 		CHECK(to_send == std::string(bytes.begin(), bytes.end()));
+
+		connections.back().close();
 	}
 	{
 		auto client = RawSocket::new_socket(client_port);
@@ -66,11 +72,13 @@ TEST_CASE("free resources", "[connection]") {
 
 		wait_for_network_interaction();
 
-		auto connection = listener.accept_connection();
-		connection.write(to_send);
+		connections.push_back(std::move(listener.accept_connection()));
+		connections.back().write(to_send);
 		wait_for_network_interaction();
 		auto bytes = client.read(1024);
 
 		CHECK(to_send == std::string(bytes.begin(), bytes.end()));
+
+		connections.back().close();
 	}
 }
