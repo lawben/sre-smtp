5a43740be9fd31b9fe1e83bdfcd29305183c30ca
martin <martin.fischer@student.hpi.de>
2018-05-20 10:08:50 +0200

    remove lying comment

--- src/smtp-lib/socket_listener.cpp
+++ src/smtp-lib/socket_listener.cpp
@@ -14,7 +14,6 @@ SocketListener::SocketListener(const uint16_t port) : m_port(port), m_raw_socket
 }
 
 std::unique_ptr<Connection> SocketListener::accept_connection() {
-    // This call blocks until a connection comes in
     auto raw_socket_conn = std::make_unique<RawSocket>(m_raw_socket.accept());
 	return std::make_unique<Connection>(std::move(raw_socket_conn));
 }
